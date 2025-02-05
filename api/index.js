4a3753baf9a4a68c24f8bb6162c482e5d6875eac
Sukriti1604 <sukritinautiyal16@gmail.com>
2025-02-05 12:37:41 +0530

    commit changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then (
+  ()=>{
+    console.log('Connected to MongoDB');
+  })
+  .catch((error)=>{
+    console.log('Error connecting to MongoDB', error.message);
+  })
 const app = express();
 
 app.listen(3000, () => {
