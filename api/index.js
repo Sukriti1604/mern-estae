d1b5d7df25fbddb819560920da2b27e4af4c1416
Sukriti1604 <sukritinautiyal16@gmail.com>
2025-02-10 13:42:01 +0530

    middleware & function to handle errors

--- api/index.js
+++ api/index.js
@@ -5,25 +5,34 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then (
-  ()=>{
+mongoose.connect(process.env.MONGO)
+  .then(() => {
     console.log('Connected to MongoDB');
   })
-  .catch((error)=>{
+  .catch((error) => {
     console.log('Error connecting to MongoDB', error.message);
-  })
+  });
 
-  
 const app = express();
 app.use(express.json());
 
+// Routes
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
+
+// Error-handling middleware (after routes)
+app.use((err, req, res, next) => {
+  // Set default values in case the error object doesn't contain them
+  res.statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  return res.status(res.statusCode).json({
+    success: false,
+    statusCode: res.statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
-}
-);
-
-
-
-app.use("/api/user", userRouter);
-app.use('/api/auth',authRouter);
- 
+});
