e5cc84c6a9007fcd3609622bc276444cb88ff95f
Sukriti1604 <sukritinautiyal16@gmail.com>
2025-02-12 14:12:42 +0530

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+/*import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import {store} from './redux/store.js'
+import {Provider} from 'react-redux'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>,
+)*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
